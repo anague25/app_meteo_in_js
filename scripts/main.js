9ca8bfaba98e99e8db1968ec5821eb5fb7b23119
Anague Sonna Roosvelt <anaguesonnaroosvelt@gmail.com>
2023-12-20 06:54:41 +0100

    app for weather final version

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 const CLEFAPI = '2ca7505a8893eea0813b1632eda08993';
+const country = document.querySelector('.country');
+const climates = document.querySelector('.climates');
+const temperature = document.querySelector('.temperature');
+const localisation = document.querySelector('.localisation');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
 
 let resultatApi ;
 
@@ -16,16 +22,23 @@ if(navigator.geolocation){
 }
 
 
-function appelApi(long,lat){
-    // console.log(long,lat);
-    // fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&exclude=minutely&units=metric&lang=fr&appid='+CLEFAPI).then((response)=> {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&exclude=minutely&appid='+CLEFAPI).then((response)=> {
 
-        return response.json;
+function appelApi(long,lat){
+//     // console.log(long,lat);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEFAPI}`).then((response)=> {
+
+        return response.json();
     }).then((data) => {
-        console.log(data);
+       let responseApi = data;
+       console.log(responseApi);
+       country.innerHTML = responseApi.sys.country;
+       wind.innerHTML = responseApi.wind.speed+' Km/h';
+       humidity.innerHTML = responseApi.main.humidity+' %';
+       localisation.innerHTML = responseApi.name;
+       temperature.innerHTML =Math.trunc(responseApi.main.temp)+' °C';
     });
 
 }
+
 
 
